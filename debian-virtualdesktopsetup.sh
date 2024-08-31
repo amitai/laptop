66cbd3a1c11da7dee9f35dfe36936a0f83f3df60
amitai <dan@brandtlautman.com>
2024-08-31 17:48:48 -0400

    Update debian-virtualdesktopsetup.sh

--- debian-virtualdesktopsetup.sh
+++ debian-virtualdesktopsetup.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
 
+echo -n "Enter new user name:"
+read newusername
+
+adduser $newusername --gecos ""
+usermod -aG sudo $newusername
+echo $newusername' ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers
+
+su $newusername
+
+apt-get update
+apt-get --yes --force-yes install iftop htop iotop traceroute git openssh-server vim curl wget lynx 
+
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)" --prefix=/usr/local --unattended
+touch ~/.hushlogin
+
+# tasksel install desktop
+apt --yes --force-yes install gnome-tweak-tool font-manager xsel aspell-en firefox-esr 
